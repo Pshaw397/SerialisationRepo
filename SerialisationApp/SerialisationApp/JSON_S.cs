efb72b26c9d8c392c83aa7f89072a870ce41399e
Pshaw397 <phillip.a.shaw@googlemail.com>
2021-04-26 20:50:44 +0100

    Added XML functionality

--- SerialisationApp/SerialisationApp/JSON_S.cs
+++ SerialisationApp/SerialisationApp/JSON_S.cs
@@ -1,40 +1,52 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+﻿//using Newtonsoft.Json;
+//using System;
+//using System.IO;
+//using System.Runtime.Serialization.Formatters.Binary;
 
-namespace SerialisationApp
-{
-    class JSON_S
-    {
-        static readonly string _path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        static void Main(string[] args)
-        {
-            //create a trainee
-            Trainee tl = new Trainee { FirstName = "Cathy", LastName = "French", SpartaNo = 100 };
-            SerializeToFileJSON(tl);
-            Trainee t2 = DeserializeFromFileJSON("SerialisationApp.Trainee.json");
-        }
+//namespace SerialisationApp
+//{
+//    class JSON_S
+//    {
+//        static readonly string _path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+//        static void Main(string[] args)
+//        {
+//            //create a trainee
+//            Trainee t1 = new Trainee { FirstName = "Cathy", LastName = "French", SpartaNo = 100 };
+//            SerializeToFileJSON(t1);
 
-        private static Trainee DeserializeFromFileJSON(string fileName)
-        {
-            // create the file path
-            var filePath = $"{_path}/{fileName}";
-            // create a Stream object for reading
-            Stream file = File.OpenRead(filePath);
-            // create the BinaryFormatter and use it
-            BinaryFormatter reader = new BinaryFormatter();
-            var trainee = (Trainee)reader.Deserialize(file);
-            file.Close();
-            return trainee;
-        }
+//            // Trainee t2 = DeserializeFromFileJSON("100 - Cathy French.json");
 
-        private static void SerializeToFileJSON(Object o)
-        {
-            // create the file path
-            var filePath = $"{_path}/{o}.json";
-            string jsonString = JsonConvert.SerializeObject(o);
-            File.WriteAllText(filePath, jsonString);
-        }
-    }
-}
+//            // create a course
+//            Course eng83 = new Course { Title = "Engineering 83", Subject = "C# SDET", StartDate = new DateTime(2021, 3, 16) };
+//            eng83.AddTrainee(t1);
+//            eng83.AddTrainee(new Trainee { FirstName = "Martin", LastName = "Beard", SpartaNo = 101});
+//            SerializeToFileJSON(eng83);
+
+//            Course otherCourse = DeserializeCourseFromFileJSON("NewCourse.json");
+//        }
+
+//        private static Trainee DeserializeFromFileJSON(string fileName)
+//        {
+//            // create the file path
+//            var filePath = $"{_path}/{fileName}";
+//            string jsonString = File.ReadAllText(filePath);
+//            return JsonConvert.DeserializeObject<Trainee>(jsonString);
+//        }
+//        private static Course DeserializeCourseFromFileJSON(string fileName)
+//        {
+//            // create the file path
+//            var filePath = $"{_path}/{fileName}";
+//            string jsonString = File.ReadAllText(filePath);
+//            return JsonConvert.DeserializeObject<Course>(jsonString);
+//        }
+
+
+//        private static void SerializeToFileJSON(Object o)
+//        {
+//            // create the file path
+//            var filePath = $"{_path}/{o}.json";
+//            string jsonString = JsonConvert.SerializeObject(o);
+//            File.WriteAllText(filePath, jsonString);
+//        }
+//    }
+//}
--- SerialisationApp/SerialisationApp/Trainee.cs
+++ SerialisationApp/SerialisationApp/Trainee.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SerialisationApp
@@ -12,6 +13,7 @@ namespace SerialisationApp
         public string FirstName { get; init; }
         public string LastName { get; init; }
         public int SpartaNo { get; init; }
+        [JsonIgnore]
         public string FullName => $"{FirstName} {LastName}";
         public override string ToString()
         {
@@ -24,13 +26,13 @@ namespace SerialisationApp
     {
         public string Subject { get; set; }
         public string Title { get; set; }
-        public DateTime startDate { get; set; }
+        public DateTime StartDate { get; set; }
         public List<Trainee> Trainees { get; } = new List<Trainee>();
         [field: NonSerialized]
         private readonly DateTime _lastRead;
         public Course()
         {
-            _lastRead = DateTime.Now
+            _lastRead = DateTime.Now;
         }
         public void AddTrainee(Trainee newTrainee)
         {
